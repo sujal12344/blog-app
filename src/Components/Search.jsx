0c560c740e8046e30f956c46f1166e4de07acabe
sujal <sujalkesharwani220@gmail.com>
2023-11-26 12:45:12 +0530

    remove

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import banner from './../assets/Images/banner.jpg'
 import { IoSearchOutline } from "react-icons/io5";
-import GlobalApi from '../Services/GlobalApi';
+
 function Search({selectedTag}) {
     const tags=[
         {
