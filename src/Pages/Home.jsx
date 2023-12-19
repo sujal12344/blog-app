c4fbf282f1190ef2546c895c5532a90e39471b85
sujal <sujalkesharwani220@gmail.com>
2023-12-19 12:45:12 +0530

    home-updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import Header from '../Components/Header'
 import Search from '../Components/Search'
 import IntroPost from '../Components/IntroPost'
 import Blogs from '../Components/Blogs'
-import Footer from '../Components/Footer'
 import GlobalApi from '../Services/GlobalApi'
 
 function Home() {
